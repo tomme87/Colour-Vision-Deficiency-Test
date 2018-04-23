af788130e086992da34cf96b4e4ffc80eecf5d84
Sjur <sssagen@hotmail.com>
2018-04-23 14:08:31 +0200

    removed dummy data in activity

--- app/src/main/java/no/ntnu/imt3673/group2/colourvisiondeficiencytest/ishihara/IshiharaTestActivity.java
+++ app/src/main/java/no/ntnu/imt3673/group2/colourvisiondeficiencytest/ishihara/IshiharaTestActivity.java
@@ -28,43 +28,6 @@ public class IshiharaTestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ishihara_test);
 
-        testInfo = new TestInfo(
-                "ishi1",
-                "Ishihara test 1",
-                "This is the first ishihara test",
-                "www.example.com",
-                "www.example.com",
-                1,
-                "ishihara",
-                1,
-                (long) 1,
-                true
-        );
-        IshiharaPlate plate = new IshiharaPlate();
-        plate.setId(1);
-        plate.setFilename("filename to plate");
-        plate.setNormal(12);
-        plate.setDeutanStrong(12);
-        plate.setProtanStrong(12);
-        plate.setTotal(12);
-        plate.setExtra(false);
-
-        plates.add(plate);
-
-        plate = new IshiharaPlate();
-        plate.setId(2);
-        plate.setFilename("filename to plate2");
-        plate.setNormal(10);
-        plate.setDeutanStrong(15);
-        plate.setProtanStrong(15);
-        plate.setTotal(null);
-        plate.setExtra(false);
-
-        plates.add(plate);
-
-        this.test.setInfo(testInfo);
-        this.test.setPlates(plates);
-
         Log.v(TAG, "Hello from ishihara test activity");
         runIshiharaTestFragment();
     }
