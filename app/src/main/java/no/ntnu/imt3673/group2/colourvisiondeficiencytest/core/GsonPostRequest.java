2d7035db0e29c781a621a846fb056d4e9c50e87a
tomme87 <tomrf@stud.ntnu.no>
2018-04-30 15:35:04 +0200

    send results to resultsUrl

--- app/src/main/java/no/ntnu/imt3673/group2/colourvisiondeficiencytest/core/GsonPostRequest.java
+++ app/src/main/java/no/ntnu/imt3673/group2/colourvisiondeficiencytest/core/GsonPostRequest.java
@@ -3,9 +3,9 @@ package no.ntnu.imt3673.group2.colourvisiondeficiencytest.core;
 import com.android.volley.AuthFailureError;
 import com.android.volley.NetworkResponse;
 import com.android.volley.ParseError;
-import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.JsonRequest;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 
@@ -13,25 +13,19 @@ import java.io.UnsupportedEncodingException;
 import java.util.Map;
 
 /**
- * From https://developer.android.com/training/volley/request-custom.html#java
+ * From https://github.com/Sottti/OkHttp-Volley-Gson/blob/master/app/src/main/java/com/sotti/okhttpvolleygson/data/GsonPostRequest.java
  */
 
-public class GsonGetRequest<T> extends Request<T> {
+public class GsonPostRequest<T> extends JsonRequest<T> {
     private final Gson gson = new Gson();
     private final Class<T> clazz;
     private final Map<String, String> headers;
     private final Response.Listener<T> listener;
 
-    /**
-     * Make a GET request and return a parsed object from JSON.
-     *
-     * @param url URL of the request to make
-     * @param clazz Relevant class object, for Gson's reflection
-     * @param headers Map of request headers
-     */
-    public GsonGetRequest(String url, Class<T> clazz, Map<String, String> headers,
-                          Response.Listener<T> listener, Response.ErrorListener errorListener) {
-        super(Method.GET, url, errorListener);
+    public GsonPostRequest(String url, String requestBody, Class<T> clazz, Map<String, String> headers,
+                           Response.Listener<T> listener, Response.ErrorListener errorListener) {
+        super(Method.POST, url, requestBody, listener, errorListener);
+
         this.clazz = clazz;
         this.headers = headers;
         this.listener = listener;
