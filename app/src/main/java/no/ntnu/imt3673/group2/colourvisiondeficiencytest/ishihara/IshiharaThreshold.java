d14a947175c73ed2ba30786efdb05eed6595a876
TomaszRudowski <tomaszmr@stud.ntnu.no>
2018-04-30 14:17:10 +0200

    added threshols.json to zip and adjusted results interpretation

---
+++ app/src/main/java/no/ntnu/imt3673/group2/colourvisiondeficiencytest/ishihara/IshiharaThreshold.java
@@ -0,0 +1,29 @@
+package no.ntnu.imt3673.group2.colourvisiondeficiencytest.ishihara;
+
+/**
+ * Created by Reyn on 30-Apr-18.
+ */
+
+public class IshiharaThreshold {
+    private Integer normal;
+    private Integer deficiency;
+
+    public IshiharaThreshold() {
+    }
+
+    public Integer getNormal() {
+        return normal;
+    }
+
+    public void setNormal(Integer normal) {
+        this.normal = normal;
+    }
+
+    public Integer getDeficiency() {
+        return deficiency;
+    }
+
+    public void setDeficiency(Integer deficiency) {
+        this.deficiency = deficiency;
+    }
+}
